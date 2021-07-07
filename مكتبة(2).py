1e95e77e947ea714002341648f3d6f1c3d7abd6c
app17A <87045382+app17A@users.noreply.github.com>
2021-07-07 04:21:18 +0300

    Add files via upload

---
+++ مكتبة(2).py
@@ -0,0 +1,7 @@
+import os
+print('جاري التثبيت')
+os.system('pip install pyfiglet')
+os.system('pip install requests')
+os.system('pip install os')
+os.system('pip install user_agent')
+os.system('cls' if os.name == 'nt' else 'clear')
